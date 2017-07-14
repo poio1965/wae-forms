171a07f30b22c68ae03070d7c2cf5b200063e5eb
Oriol Rocafort <oriol.rocafort@cantera-tech.com>
2017-07-14 13:06:25 +0200

    [bug] typo

--- web/client/src/app/components/restaurant-template/restaurant-template.component.spec.ts
+++ web/client/src/app/components/restaurant-template/restaurant-template.component.spec.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:no-unused-variable */
 import { TestBed, async } from '@angular/core/testing';
-import { RestaurantTemplateComponent } from './restaurant-template.component
+import { RestaurantTemplateComponent } from './restaurant-template.component';
 describe('AppComponent', () => {
 	beforeEach(() => {
 		TestBed.configureTestingModule({
